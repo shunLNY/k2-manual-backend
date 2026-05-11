130c952ba118c46cdfebf5f836f2335e151edf63
ShunLNY <shunlae@o-technique-myanmar.com>
2026-05-11 13:38:42 +0630

    db setup

--- src/modules/categories/dto/create-category.dto.ts
+++ src/modules/categories/dto/create-category.dto.ts
@@ -1,1 +1,36 @@
-export class CreateCategoryDto {}
+import { IsEnum, IsNotEmpty, IsNumber, IsString, MaxLength } from "class-validator";
+import { StatusType } from "src/common/constants";
+
+export class CreateCategoryDto {
+  @IsNumber()
+  sort_order: number;
+
+  @IsEnum(StatusType)
+  status: StatusType;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  category_name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  category_slug: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(50)
+  thumbnail_path: string;
+
+  @IsString()
+  creator_id: string;
+
+  @IsString()
+  editor_id: string;
+
+}
